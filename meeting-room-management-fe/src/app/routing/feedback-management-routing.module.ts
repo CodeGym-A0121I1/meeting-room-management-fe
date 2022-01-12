8559b216d3f879aefa06d16762adc8d7e89fce3f
Your Name <you@example.com>
2022-01-12 20:38:38 +0700

    Update List-Feedback

--- meeting-room-management-fe/src/app/routing/feedback-management-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/feedback-management-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListFeedbackComponent} from "../component/feedback-management/list-feedback/list-feedback.component";
 
 const routes: Routes = [
   {path: "feedback", children: [
-
+      {path: "", component: ListFeedbackComponent}
     ]}
 ];
 
