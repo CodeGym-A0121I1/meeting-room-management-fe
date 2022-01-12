b3c2b6896c3988e5b5ce168be1dfec277901100a
phamquynhnhumm <quynhnhuctcn@gmail.com>
2022-01-12 23:42:03 +0700

    update signup-room-NhuPTQ

--- meeting-room-management-fe/src/app/app.module.ts
+++ meeting-room-management-fe/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './routing/app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
@@ -12,8 +11,12 @@ import {MatNativeDateModule} from "@angular/material/core";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatInputModule} from "@angular/material/input";
 import {NgxPaginationModule} from "ngx-pagination";
-import {RouterModule} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {RoomManagementModule} from "./component/room-management/room-management.module";
+import {SigupRoomComponent} from "./component/registration-history-management/sigup-room/sigup-room.component";
+const routesConfig: Routes = [
+  {path: 'signuproom', component:  SigupRoomComponent},
+];
 
 @NgModule({
   declarations: [
@@ -33,7 +36,8 @@ import {RoomManagementModule} from "./component/room-management/room-management.
     MatInputModule,
     MatSnackBarModule,
     NgxPaginationModule,
-    RoomManagementModule
+    RoomManagementModule,
+    RouterModule.forRoot(routesConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
