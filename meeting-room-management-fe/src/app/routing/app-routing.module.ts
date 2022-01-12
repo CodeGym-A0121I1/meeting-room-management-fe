7343c49d1f39c954ccf5b573cf6718ba54355237
Your Name <you@example.com>
2022-01-12 12:09:37 +0700

    Update sub-branch Feedback-Management

--- meeting-room-management-fe/src/app/routing/app-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FeedbackManagementModule} from "../component/feedback-management/feedback-management.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "feedback", component: FeedbackManagementModule, children:[]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
