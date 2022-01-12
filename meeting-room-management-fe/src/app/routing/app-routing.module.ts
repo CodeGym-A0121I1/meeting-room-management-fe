ed62b50c3fcde6662bf5ef4cbd24ea4c43abc5d7
Your Name <you@example.com>
2022-01-12 21:57:09 +0700

    Update sub-branch feedback-management

--- meeting-room-management-fe/src/app/routing/app-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FeedbackManagementModule} from "../component/feedback-management/feedback-management.module";
 
 const routes: Routes = [
-  {
-    path: 'feedback',
-    redirectTo: 'feedback',
-    pathMatch: 'full'
+  {path: 'feedback', redirectTo: 'feedback', pathMatch: 'full'
   }
 ];
 
--- meeting-room-management-fe/src/app/routing/feedback-management-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/feedback-management-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: "feedback", children: [
-
-    ]}
+  {path: "feedback", children: []}
 ];
 
 @NgModule({
