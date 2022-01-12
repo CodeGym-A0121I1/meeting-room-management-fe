46cfea6acee6dc3ee8fc6fe6a69faae2c39b8896
Your Name <you@example.com>
2022-01-12 21:54:34 +0700

    Update List-Feedback

--- meeting-room-management-fe/src/app/routing/app-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {EquipmentManagementModule} from "../component/equipment-management/equipment-management.module";
 
 const routes: Routes = [
-  {path: "equipment", component: EquipmentManagementModule, children: [] }
+  {
+    path: 'equipment',
+    redirectTo: 'equipment',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
--- meeting-room-management-fe/src/app/routing/equipment-management-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/equipment-management-routing.module.ts
@@ -3,7 +3,9 @@ import {RouterModule, Routes} from '@angular/router';
 import {ListEquipmentComponent} from "../component/equipment-management/list-equipment/list-equipment.component";
 
 const routes: Routes = [
-  {path: ":idCategory", component: ListEquipmentComponent}
+  {path: "equipment", children: [
+      {path: "", component: ListEquipmentComponent}
+    ]}
 ];
 
 @NgModule({
