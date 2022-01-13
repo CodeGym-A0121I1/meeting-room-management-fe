9115cffd51b8d32e0079337dfc78b64d5791afc6
ThangDoan1 <winhkdn@gmail.com>
2022-01-13 15:17:17 +0700

    Update sub-branch user-management

--- meeting-room-management-fe/src/app/service/user.service.ts
+++ meeting-room-management-fe/src/app/service/user.service.ts
@@ -1,9 +1,22 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  URL_API = "http://localhost:8080/api/users";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0cm9uZyIsImV4cCI6MTY0MjA5NTc2OCwiaWF0IjoxNjQyMDU5NzY4fQ.pgaDLmamzjb6kiSFexDxIwThwGs4JXLftjcqdqoF0NtGEm7xkR5dMe68XZmk1RvN9qOSp7xU_y5Xrga9cDs3eA'
+    })
+  };
+
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
+
 }
