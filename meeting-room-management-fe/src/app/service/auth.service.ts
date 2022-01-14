957f46c4d463e6b562dd20def9ab69172fb2b296
phuongtt <ttphuong172@gmail.com>
2022-01-14 17:14:25 +0700

    Create Login

---
+++ meeting-room-management-fe/src/app/service/auth.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() {
+  }
+
+  public setToken(jwtToken: string) {
+    localStorage.setItem('jwtToken', jwtToken);
+  }
+
+  public getToken() {
+    return localStorage.getItem('jwtToken');
+  }
+
+  public setRole(role: string) {
+    localStorage.setItem("role", role)
+  }
+
+  public getRole() {
+    return localStorage.getItem('role')
+  }
+}
