237f15543632f318977f08ddc49d2db81ed8ac04
ThangDoan1 <winhkdn@gmail.com>
2022-01-19 01:01:04 +0700

    Update change-password-user1-ThangDM

--- meeting-room-management-fe/src/app/routing/app-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path : "user", redirectTo : "abc", pathMatch : "full"}
+  {path: "user", redirectTo: "user", pathMatch: "full"},
+
+  {path: "changePassword", redirectTo: "changePassword", pathMatch: "full"}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- meeting-room-management-fe/src/app/routing/user-management-routing.module.ts
+++ meeting-room-management-fe/src/app/routing/user-management-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChangePasswordUserComponent} from "../component/user-management/change-password-user/change-password-user.component";
 
 const routes: Routes = [
   {path:"user" , children:[
 
+    ]},
+  {path:"changePassword" , children:[
+      {path : "",component: ChangePasswordUserComponent}
     ]}
 ];
 
