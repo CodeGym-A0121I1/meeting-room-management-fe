41c541a1925d41c57c968f203e8ddd5dbf3565f6
Lomahs <thanh.bui.bao@gmail.com>
2022-01-20 22:04:42 +0700

    Add equipment management, authorization, 404 and forbidden page

--- meeting-room-management-fe/src/app/service/room.service.ts
+++ meeting-room-management-fe/src/app/service/room.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Floor} from "../model/room/Floor";
 import {Area} from "../model/room/Area";
 import {RoomType} from "../model/room/RoomType";
 import {Room} from "../model/room/Room";
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,8 @@ import {Room} from "../model/room/Room";
 export class RoomService {
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private authService: AuthService
   ) {
   }
 
@@ -21,25 +23,30 @@ export class RoomService {
   private readonly api_area = "http://localhost:8080/api/rooms/areas";
   private readonly api_roomType = "http://localhost:8080/api/rooms/roomTypes";
   private readonly api_image = "http://localhost:8080/api/rooms/image";
+  private readonly JWT = this.authService.getToken() || "";
+
+  headers = new HttpHeaders({
+    'Authorization': this.JWT
+  })
 
   getAllFloors(): Observable<Array<Floor>> {
-    return this.httpClient.get<Array<Floor>>(this.api_floor);
+    return this.httpClient.get<Array<Floor>>(this.api_floor, {headers: this.headers});
   }
 
   getAllAreas(): Observable<Array<Area>> {
-    return this.httpClient.get<Array<Area>>(this.api_area);
+    return this.httpClient.get<Array<Area>>(this.api_area, {headers: this.headers});
   }
 
   getAllRoomTypes(): Observable<Array<RoomType>> {
-    return this.httpClient.get<Array<RoomType>>(this.api_roomType);
+    return this.httpClient.get<Array<RoomType>>(this.api_roomType, {headers: this.headers});
   }
 
   addRoom(room: Room) {
-    return this.httpClient.post(this.api_room, room);
+    return this.httpClient.post(this.api_room, room, {headers: this.headers});
   }
 
   getImageName(): Observable<string> {
     // @ts-ignore
-    return this.httpClient.get<string>(this.api_image, {responseType: 'text'});
+    return this.httpClient.get<string>(this.api_image, {responseType: 'text', headers: this.headers});
   }
 }
